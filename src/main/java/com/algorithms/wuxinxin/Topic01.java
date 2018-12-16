871924a3fecff520aaeb7acc2d740425131c06c0
wuxinxin-personal <45267308+wuxinxin-personal@users.noreply.github.com>
2018-12-16 20:10:08 +0800

    z

--- src/main/java/com/algorithms/wuxinxin/Topic01.java
+++ src/main/java/com/algorithms/wuxinxin/Topic01.java
@@ -12,4 +12,33 @@ package com.algorithms.wuxinxin;
  * Created by Administrator on 2018/12/16/016.
  */
 public class Topic01 {
+
+    public static void main(String[] args) {
+        int[]  numer={2,3,9,5,4};
+
+        System.out.println(getMaxValueIndex(numer,0,4));
+    }
+
+    public  static void   binarySearch(int[]array,int start,int end,int n){
+       // if(start==end)
+    }
+
+    //获取最大值
+    public static int getMaxValueIndex(int[]array,int start,int end){
+        int index=0;
+        int max=0;
+
+        for(int i=start;i<=end;i++){
+            if(array[i]>max){
+                max=array[i];
+                index=i;
+            }
+
+            if(max==9){
+                return index;
+            }
+        }
+
+        return index;
+    }
 }
