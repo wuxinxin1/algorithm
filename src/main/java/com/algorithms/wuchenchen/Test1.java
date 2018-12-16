496af64c5fe32663aa89c6c813d0e202e3602295
wuxinxin-personal <45267308+wuxinxin-personal@users.noreply.github.com>
2018-12-16 20:13:43 +0800

    z

---
+++ src/main/java/com/algorithms/wuchenchen/Test1.java
@@ -0,0 +1,10 @@
+package com.algorithms.wuchenchen;
+
+/**
+ * Created by Administrator on 2018/12/16/016.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
